1cff5e2f7119f21c08c3645cf4325942a1588bd1
Tim Stewart <tim@stoo.org>
2011-12-10 15:39:09 -0500

    Add quick and dirty unit test

---
+++ src/tqueue_test.erl
@@ -0,0 +1,30 @@
+-module(tqueue_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%% Quick and dirty test
+in_out_len_test() ->
+    Q = tqueue:new(),
+
+    Item1 = "Item One",
+    Item2 = "item Two",
+    Item3 = "item Three",
+
+    0 = tqueue:len(Q),
+    Q1 = tqueue:in(Q, Item1),
+    1 = tqueue:len(Q1),
+    Q2 = tqueue:in(Q1, Item2),
+    2 = tqueue:len(Q2),
+    Q3 = tqueue:in(Q2, Item3),
+    3 = tqueue:len(Q3),
+
+    {ok, Q4, Item1} = tqueue:out(Q3),
+    2 = tqueue:len(Q4),
+    {ok, Q5, Item2} = tqueue:out(Q4),
+    1 = tqueue:len(Q5),
+    {ok, Q6, Item3} = tqueue:out(Q5),
+    0 = tqueue:len(Q6),
+
+    {empty, Q6} = tqueue:out(Q6),
+
+    ok.
